6f90cb68279973cceb5fec7b5b7cd2367b7f6806
Ian Stigliano Silva <ianstigli@hotmail.com>
2021-11-01 18:37:49 -0300

    no errors

--- chat/mandar-msg.php
+++ chat/mandar-msg.php
@@ -6,7 +6,6 @@ if(!isset($_SESSION)) {
 $destinatario = 0;
     if(isset($_POST['destino'])){
     $destinatario = $_POST['destino'];
-    }
     if ($destinatario != 0){
     $busca_id_destino = mysqli_query($conn, "SELECT id_usuario from tb_usuario where nome = '$destinatario'");
     $busca = mysqli_fetch_array($busca_id_destino);
@@ -15,14 +14,15 @@ $destinatario = 0;
     $id_remetente = $_SESSION['id'];
     print_r('ID REMETENtE: ' . $id_remetente);
 
+    
     $mensagem = $_POST['msg'];
 
     }
        
     include ('../sistemas/sistema_conexao.php');
-    $destinatario = $_POST['destino'];
 
     if($mensagem != "" or $id_destinatario != ""){
     $sql =  mysqli_query($conn ,"INSERT INTO chat2(remetente_id, destinatario_id, msg) VALUES('$id_remetente', '$id_destinatario', '$mensagem')");
     }
+}
     ?>
